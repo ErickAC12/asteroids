7e7ddc2202ecb85b3dfce8c5742deff5287be8f6
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-10-20 20:35:54 -0600

    asteroids split when hit, and disappear when too small

--- main.py
+++ main.py
@@ -30,15 +30,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         for obj in updatable:
             obj.update(dt)
+
         for asteroid in asteroids:
             if asteroid.isColliding(player):
                 print('Game over!')
                 return
+            for shot in shots:
+                if shot.isColliding(asteroid):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill('black')
+
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
