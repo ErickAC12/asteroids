66616999f88c44df7c951908a580b8b83b560ee9
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-10-20 18:50:13 -0600

    rotate left and right

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -12,8 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.update(dt)
 
 
 if __name__ == "__main__":
