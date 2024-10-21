3b6888f1295674104e1f8698e9ce4ac5089c2240
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-10-20 19:10:21 -0600

    objects are in groups 'updatable' and 'drawable'

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ from player import Player
 
 def main():
     pygame.init()
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # game loop
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
         screen.fill('black')
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
 
 
 if __name__ == "__main__":
